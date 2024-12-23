086c299bb694a94bc3d1b60b7bde050fa8a9be74
Abderraouf Gaouer <93593515+GaouerRaouf@users.noreply.github.com>
2024-12-23 15:09:58 +0100

    [fix[ renamed train hit event

--- Assets/_Scripts/Obstacles/ObstacleHit.cs
+++ Assets/_Scripts/Obstacles/ObstacleHit.cs
@@ -4,12 +4,12 @@ using UnityEngine.Events;
 using UnityEditor.Build.Content;
 public class ObstacleHit : MonoBehaviour
 {
-    public UnityEvent triggerAction;
+    public UnityEvent onTrainHit;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Train"))
         {
-            triggerAction?.Invoke();
+            onTrainHit?.Invoke();
             // GameManager.instance.health--;
             // other.gameObject.GetComponent<Health>().currentHealth--;
             FadeManager.Instance.FadeInOut();   
